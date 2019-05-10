5e466b4a503e2acb52d858d01144fa837f0d2b46
Chugu <gchurlet@gmail.com>
2019-05-10 16:27:56 +0200

    Installation de vichuploader-bundle pour la gestion des upload de fichiers

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
